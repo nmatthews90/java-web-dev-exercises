9c628679ab33a67584aad025da53a19fd5de77dd
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-07 19:33:16 -0600

    added the File Chooser class

---
+++ src/org/launchcode/java/studios/countingcharacters/FileChooser.java
@@ -0,0 +1,52 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FileChooser {
+    public static void main(String[] args) throws FileNotFoundException {
+        HashMap<Character, Integer> characterCount = new HashMap<>();
+        JFileChooser chooser = new JFileChooser();
+
+//        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//                "Text Files", "txt");
+//
+//        chooser.setFileFilter(filter);
+
+        int returnVal = chooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+
+            System.out.println(chooser.getSelectedFile().getAbsolutePath());
+
+            String fileToUse = chooser.getSelectedFile().getAbsolutePath();
+            File myObj = new File(fileToUse);
+            Scanner phraseInput;
+            phraseInput = new Scanner(myObj);
+
+            String phraseToUse = phraseInput.nextLine();
+
+            String phraseCaseInsensitive = phraseToUse.toLowerCase();
+            String newReplacedString = phraseCaseInsensitive.replaceAll("[^a-z]", "");
+
+
+            char[] charactersInString = newReplacedString.toCharArray();
+
+            for (char letters : charactersInString) {
+                if (characterCount.containsKey(letters)) {
+                    characterCount.put(letters, characterCount.get(letters) + 1);
+                } else {
+                    characterCount.put(letters, 1);
+                }
+
+            }
+            for(Map.Entry<Character, Integer> oneChar : characterCount.entrySet()) {
+                System.out.println(oneChar.getKey() + ":" + oneChar.getValue());
+            }
+        }
+    }
+
+}
--- src/org/launchcode/java/studios/countingcharacters/PhraseToCount.java
+++ src/org/launchcode/java/studios/countingcharacters/PhraseToCount.java
@@ -1,17 +1,33 @@
 package org.launchcode.java.studios.countingcharacters;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class PhraseToCount {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
-        String phraseToUse = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+        String fileToUse = "C:\\Users\\Natasha\\Documents\\lc101\\java-practice\\java-web-dev-exercises\\src\\org\\launchcode\\java\\studios\\countingcharacters\\texttouse.txt";
+        File myObj = new File(fileToUse);
+        Scanner phraseInput = new Scanner(myObj);
+
+
+        System.out.print("Enter phrase to parse: ");
+        String phraseToUse = phraseInput.nextLine();
+
+        /*String phraseToUse = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
                 "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
                 "Once you’ve done that, it’s pretty straightforward from there.";
+        */
+        String phraseCaseInsensitive = phraseToUse.toLowerCase();
+        String newReplacedString = phraseCaseInsensitive.replaceAll("[^a-z]", "");
 
-        char[] charactersInString = phraseToUse.toCharArray();
+
+        char[] charactersInString = newReplacedString.toCharArray();
 
         for (char letters : charactersInString) {
             if (characterCount.containsKey(letters)) {
