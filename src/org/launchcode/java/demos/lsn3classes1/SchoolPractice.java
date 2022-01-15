24c2446bb9ac35fd6e067e77e55c6e1e93c92d99
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-15 11:53:37 -0600

    did chapter 4 work

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,12 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student newStudent = new Student("Natasha", 1, 1, 4.0);
+        System.out.println(newStudent.studentInfo());
+        newStudent.setName("Natasha Matthews");
+        System.out.println(newStudent.getName());
     }
 }
--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -10,6 +10,15 @@ public class Course {
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
 
+    @Override
+    public String toString() {
+        return "Course{" +
+                "topic='" + topic + '\'' +
+                ", instructor=" + instructor +
+                ", enrolledStudents=" + enrolledStudents +
+                '}';
+    }
+
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
