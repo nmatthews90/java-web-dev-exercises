430fe865d9016760dad40ff07e6f20b93fdc27f0
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-30 10:54:35 -0600

    did the interface exercise

--- src/exercises/technology/AbstractEntity.java
+++ src/exercises/technology/AbstractEntity.java
@@ -1,5 +1,16 @@
 package exercises.technology;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class AbstractEntity {
 
+    private int ID;
+
+    public AbstractEntity() {
+        this.ID = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -7,12 +7,21 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        //Comparator<Flavor> comparator = new FlavorComparator();
+        //flavors.sort(comparator);
+
+
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
 
+        flavors.sort(new FlavorComparator());
+
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
